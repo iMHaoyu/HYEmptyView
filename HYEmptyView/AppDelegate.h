39a608923576e9aa2e2d0eba30db2da0b18aca49
徐浩宇 <mac_xu@xuhaoyudemacbook-pro.local>
2018-03-02 15:11:22 +0800

    Initial Commit

---
+++ HYEmptyView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYEmptyView
+//
+//  Created by 徐浩宇 on 2018/3/2.
+//  Copyright © 2018年 徐浩宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HYEmptyView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HYEmptyView
+//
+//  Created by 徐浩宇 on 2018/3/2.
+//  Copyright © 2018年 徐浩宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
